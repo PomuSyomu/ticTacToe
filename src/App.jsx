b93ff32bf80937fa4efb0e34e5985abb1bfddf79
PomuSyomu <126451689+PomuSyomu@users.noreply.github.com>
2024-09-27 19:42:59 +0900

    usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import './style.scss';
+import { useState } from "react";
+import "./style.scss";
 import Board from "./components/Board.jsx";
 
 function App() {
+    const [counter,setCounter]=useState(1);
+    const onBtnClick = (event) => {
+        console.log("Hello", event);
+        setCounter((currentCounter)=>{
+			return currentCounter+1;
+		});
+    };
 
-  return (
-      <div className="app">
-        <Board/>
-      </div>
-  )
+    return (
+        <div className="app">
+            <div>
+                <button onClick={onBtnClick}>Click me please</button>
+				<p>{counter}</p>
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
